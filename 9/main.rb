3bbf6e4f00abe200f86b43482870dbe17757baf0
Geoffrey Donaldson <geoffrey@blackboxco.com.au>
2021-12-11 14:54:35 +1000

    Day 9 cleanup

--- 9/main.rb
+++ 9/main.rb
@@ -3,21 +3,12 @@
 input = IO.readlines('./input.txt', chomp: true)
 
 @height_map = height_map = input.map { _1.chars.map(&:to_i) }
-
-@width = width = height_map.first.length
-@depth = depth = height_map.length
-
-pp "Map of #{width} x #{depth}"
-
-low_points = []
+@width = height_map.first.length
+@depth = height_map.length
+# pp "Map of #{width} x #{depth}"
 
 def surrounding_points(y, x)
-  [
-    [y - 1, x],
-    [y, x - 1],
-    [y + 1, x],
-    [y, x + 1]
-  ]
+  [[y - 1, x], [y, x - 1], [y + 1, x], [y, x + 1]]
 end
 
 def surrounding(y, x)
@@ -28,19 +19,12 @@ def surrounding(y, x)
   end.filter { !_1[2].nil? && _1[2] != 9 }
 end
 
+low_points = []
 height_map.each.with_index do |row, y|
   row.each.with_index do |current, x|
-    # pp height_map
-
-    t = height_map.dig(y - 1, x) if y - 1 >= 0
-    l = height_map.dig(y, x - 1) if x - 1 >= 0
-    b = height_map.dig(y + 1, x) if y + 1 < depth
-    r = height_map.dig(y, x + 1) if x + 1 < width
-
+    t, l, r, b = surrounding_points(y, x).map { height_map.dig(*_1) }
     points = [t, l, r, b]
-    # pp "checking #{current} - #{points} for #{x} #{y}"
-
-    low_points << [y, x, current] if current < [t, l, r, b].compact.min
+    low_points << [y, x, current] if current < points.compact.min
   end
 end
 
@@ -58,12 +42,7 @@ def dive(point, found)
 end
 
 basins = low_points.map do |point|
-  y, x, _value = point
-  found = [point]
-
-  found = dive(point, found)
-
-  found.length
+  dive(point, [point]).length
 end
 
 pp basins.sort.reverse.take(3).reduce(&:*)
