600894cebb02c53a7b8a65024b1e3c8ec5d16d11
Geoffrey Donaldson <geoffrey@blackboxco.com.au>
2021-12-13 16:55:53 +1000

    Day 13, Part 1 & 2

---
+++ 13/main.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+input = IO.readlines('./input.txt', chomp: true)
+
+@dots, instructions = input.slice_when { _1 == '' }.to_a
+
+@dots = @dots.reject { _1 == '' }
+
+def fold_up(fold)
+  @dots.each.with_index do |dot, idx|
+    x, y = dot.split(',').map(&:to_i)
+
+    if y > fold
+      dist = y - fold
+      new_y = fold - dist
+      @dots[idx] = [x, new_y].join(',')
+    end
+  end
+end
+
+def fold_left(fold)
+  @dots.each.with_index do |dot, idx|
+    x, y = dot.split(',').map(&:to_i)
+
+    if x > fold
+      dist = x - fold
+      new_x = fold - dist
+      @dots[idx] = [new_x, y].join(',')
+    end
+  end
+end
+
+instructions.each do |instruction|
+  direction, loc = instruction.split('=')
+  direction = direction[-1]
+  case direction
+  when 'x'
+    fold_left(loc.to_i)
+  when 'y'
+    fold_up(loc.to_i)
+  end
+  # Part 1 - print on first loop
+  # pp @dots.sort
+  # pp @dots.uniq.length
+end
+
+chart = []
+
+@dots.uniq.each do |dot|
+  x, y = dot.split(',').map(&:to_i)
+  chart[y] ||= []
+  chart[y][x] = '#'
+end
+
+chart.each do |row|
+  pp row.map { _1 == '#' ? '#' : ' ' }.join('')
+end
