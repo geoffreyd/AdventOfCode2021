59689d7952da8295cf63e67b59c0616af3d1ffc4
Geoffrey Donaldson <geoffrey@blackboxco.com.au>
2021-12-14 16:05:00 +1000

    Day 14, Part 2

--- 14/day2.exs
+++ 14/day2.exs
@@ -1,4 +1,4 @@
-defmodule AdventOfCodeDay2 do
+defmodule AdventOfCodeDay do
   def part1(:sample), do: File.stream!("sample.txt") |> process1;
   def part1(:actual), do: File.stream!("input.txt") |> process1;
 
@@ -16,26 +16,26 @@ end
 
 ExUnit.start()
 
-defmodule AdventOfCodeDay2Test do
+defmodule AdventOfCodeDayTest do
   use ExUnit.Case
 
   @tag :skip
   test "part 1 works with sample data" do
-    assert AdventOfCodeDay2.part1(:sample) == 7
+    assert AdventOfCodeDay.part1(:sample) == 7
   end
 
   @tag :skip
   test "part 2 works with real data" do
-    assert AdventOfCodeDay2.part1(:actual) == 1393
+    assert AdventOfCodeDay.part1(:actual) == 1393
   end
 
   @tag :skip
   test "part 2 works with sample data" do
-    assert AdventOfCodeDay2.part2(:sample) == 5
+    assert AdventOfCodeDay.part2(:sample) == 5
   end
 
   @tag :skip
   test "part 2 works with real data" do
-    assert AdventOfCodeDay2.part2(:actual) == 1359
+    assert AdventOfCodeDay.part2(:actual) == 1359
   end
 end
---
+++ 14/main.rb
@@ -0,0 +1,67 @@
+# frozen_string_literal: true
+
+input = IO.readlines('./input.txt', chomp: true)
+
+template, pairs = input.slice_when { _1 == '' }.to_a
+
+@template, = template
+
+letters = @template.split('')
+first = letters.first
+last = letters.last
+
+@pairs = pairs.map { |pair| pair.split(' -> ') }.to_h
+
+@pair_counts = Hash.new(0)
+
+each_con2 = @template.split('').each_cons(2).map(&:join)
+
+each_con2.each do |con2|
+  @pair_counts[con2] += 1
+end
+
+pp @pair_counts
+
+pp "--- start loop ---"
+
+(0...40).each do |i|
+  new_counts = Hash.new(0)
+
+  @pair_counts.each do |pair, count|
+    a, b = pair.split('')
+    middle = @pairs[pair]
+    pair1 = [a, middle].join
+    pair2 = [middle, b].join
+    new_counts[pair1] += count
+    new_counts[pair2] += count
+  end
+  pp new_counts
+
+  @pair_counts = new_counts
+
+  # @template = @template.split('').zip(middles).flatten.join
+  # pp @template.length
+end
+
+letter_counts = Hash.new(0)
+
+@pair_counts.each do |pair, count|
+  a, b = pair.split('')
+  letter_counts[a] += count
+  letter_counts[b] += count
+end
+
+letter_counts[first] += 1
+letter_counts[last] += 1
+
+min, max = letter_counts.values.minmax
+
+pp (max/2) - (min/2)
+
+# by_letter = @template.split('').group_by { _1 }
+
+# by_letter.transform_values! { |v| v.length }
+
+# min, max = by_letter.values.minmax
+
+# pp max - min
